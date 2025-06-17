063c2c0935b3435da98bda2437adfd43e1a8cb45
Marco Mannes <marco@mmannes.com>
2025-06-17 10:21:16 -0300

    Added faster cmp and bind to delete buffers in telescope

--- init.lua
+++ init.lua
@@ -442,7 +442,16 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        pickers = {
+          buffers = {
+            sort_lastused = true,
+            mappings = {
+              n = {
+                ['d'] = 'delete_buffer',
+              },
+            },
+          },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -830,6 +839,16 @@ require('lazy').setup({
     },
   },
 
+  -- add blink.compat
+  {
+    'saghen/blink.compat',
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = '*',
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
+  },
   {
     'saghen/blink.cmp',
     -- optional: provides snippets for the snippet source
